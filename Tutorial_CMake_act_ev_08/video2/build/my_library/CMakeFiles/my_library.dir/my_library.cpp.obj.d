96d84e8cd221e578972d21347eb60799dc24db69
katiarudenick <katia.rnk@gmail.com>
2024-07-12 17:24:29 -0300

    Subo atividad tutorial cmake

---
+++ Tutorial_CMake_act_ev_08/video2/build/my_library/CMakeFiles/my_library.dir/my_library.cpp.obj.d
@@ -0,0 +1,3 @@
+my_library/CMakeFiles/my_library.dir/my_library.cpp.obj: \
+ D:\ISFT151\Ingenieria\ de\ Software\ 2\Tutorial\ CMake_act_ev_08\video2\my_library\my_library.cpp \
+ D:\ISFT151\Ingenieria\ de\ Software\ 2\Tutorial\ CMake_act_ev_08\video2\my_library\my_library.h
---
+++ Tutorial_CMake_act_ev_08/video4/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int foo() {
+        return 0;
+}
+int main()
+{
+    cout << "Hello, world!" << endl;
+    return 0;
+}
